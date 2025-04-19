7f346781bbedaabe5ce855b6f242e09f8564d46b
FAtaxi <fataxiservice@outlook.com>
2025-04-19 19:50:15 +0200

    server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 // Configure CORS
 const corsOptions = {
-  origin: 'https://jouwdomein.nl', // Vervang door jouw front-end domein
+  origin: 'https://fataxi.github.io/Onlinetest/', // Vervang door je front-end domein
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
   exposedHeaders: ['Access-Control-Allow-Private-Network'],
@@ -22,23 +22,23 @@ const authToken = 'f8b5284f7b1f9dff52c6eec96476f0f8'; // Vervang door je Twilio 
 const client = twilio(accountSid, authToken);
 
 const fromWhatsAppNumber = 'whatsapp:+14155238886'; // Twilio WhatsApp nummer
-const toWhatsAppNumber = 'whatsapp:+31647972301'; // Jouw WhatsApp nummer
 
 app.get('/', (req, res) => {
   res.send('Welkom bij de Twilio API server! Stuur een POST-verzoek naar /send om een bericht te verzenden.');
 });
 
 app.post('/send', async (req, res) => {
-  const { message } = req.body;
+  const { message, toWhatsAppNumber } = req.body; // Nu kun je ook een nummer vanuit de request krijgen
 
-  if (!message) {
-    return res.status(400).json({ success: false, error: 'Bericht wordt niet meegegeven' });
+  // Verifieer of het bericht en het nummer zijn meegegeven
+  if (!message || !toWhatsAppNumber) {
+    return res.status(400).json({ success: false, error: 'Bericht en/of nummer wordt niet meegegeven' });
   }
 
   try {
     const msg = await client.messages.create({
       from: fromWhatsAppNumber,
-      to: toWhatsAppNumber,
+      to: toWhatsAppNumber, // Gebruik het nummer dat is meegegeven in de body
       body: message,
     });
 
@@ -50,6 +50,7 @@ app.post('/send', async (req, res) => {
   }
 });
 
+// Start de server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server draait op http://localhost:${PORT}`);
